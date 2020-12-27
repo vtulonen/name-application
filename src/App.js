c38d253a6595d32ebf9a952e10af1bda4b2466ab
Vili Tulonen <tulonenvili@gmail.com>
2020-12-27 13:47:33 +0200

    getNames function to read names from json file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import React, { useState, useEffect } from 'react';
+
 function App() {
+
+  const [names, setNames] = useState([]);
+
+  const getNames=()=>{
+    fetch('names.json'
+    ,{
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    })
+      .then(response => response.json())
+      .then(data => 
+        setNames(data.names))
+    }
+
+  useEffect(()=>{
+    getNames()
+  },[])
+
   return (
-    <div className="App">
-     <p>Hello</p>
-     <code>
-       
-     This is code
-     </code>
+    <div className="app">
+     { console.log(names) }
     </div>
   );
 }
