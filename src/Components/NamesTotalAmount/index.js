59ae6ac38f7d62e76861c151dbf8c1356be41eaf
Vili Tulonen <tulonenvili@gmail.com>
2020-12-29 14:24:52 +0200

    added total amount of people to TotalNameAmount component + styles

--- src/Components/NamesTotalAmount/index.js
+++ src/Components/NamesTotalAmount/index.js
@@ -2,15 +2,34 @@ import React from 'react'
 import './styles.scss'
 
 const NamesTotalAmount = (props) => {
-  const total = props.names.length
-  
+  //Different names
+  const totalNames = props.names.length
+  //Total amount of people
+  let totalPeople = 0;
+
+  props.names.forEach(item => {
+    totalPeople += item.amount
+  })
+
+console.log(totalPeople);
+
   return (
-    <div className="results names-total">
+    <>
+    <div className="results total-names">
       <div className="result">
         <div className="result__text">Total amount of different names at Solita:</div>
-        <div className="result__amount">{total}</div>
+        <div className="result__amount">{totalNames}</div>
       </div>
     </div>
+
+    <div className="results total-people">
+      <div className="result">
+        <div className="result__text">Total amount of 
+        people at Solita:</div>
+        <div className="result__amount">{totalPeople}</div>
+      </div>
+    </div>
+    </>
   )
 }
 
